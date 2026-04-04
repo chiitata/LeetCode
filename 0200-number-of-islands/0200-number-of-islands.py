4919000690f0c22f940d42d29201f61b450a6937
TaichiSenba <chiitata3326@gmail.com>
2026-04-04 17:02:25 +0900

    Time: 249 ms (46.93%), Space: 21.7 MB (58.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,39 +1,22 @@
 from collections import deque
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        visited = [[False for _ in range(n)] for _ in range(m)]
+        if not grid:
+            return 0
+        n, m = len(grid), len(grid[0])
         count = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1" and visited[i][j] == False:
-                    que = deque()
-                    que.append((i, j))
+        direct = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == "1":
+                    count += 1
+                    que = deque([(i, j)])
                     while que:
-                        a, b = que.popleft()
-                        if visited[a][b]:
-                            continue
-                        visited[a][b] = True
-                        ans = self.is_island(grid, a, b, m, n)
-                        for t in ans:
-                            que.append(t)
-                    else:
-                        count += 1
+                        x, y = que.popleft()
+                        for xx, yy in direct:
+                            if 0<= xx+x <n and 0<= yy + y <m and grid[xx+x][yy+y] == "1":
+                                que.append((xx+x, yy+y))
+                                grid[xx+x][yy+y] = 0
         return count
-    def is_island(self, grid, a, b, m, n):
-        ans = []
-        if a+1 <= m-1:
-            if grid[a+1][b] == "1":
-                ans.append((a+1, b))
-        if a-1 >= 0:
-            if grid[a-1][b] == "1":
-                ans.append((a-1, b))
-        if b+1 <= n-1:
-            if grid[a][b+1] == "1":
-                ans.append((a, b+1))
-        if b-1 >= 0:
-            if grid[a][b-1] == "1":
-                ans.append((a, b-1))
-        return ans
+
+                    
