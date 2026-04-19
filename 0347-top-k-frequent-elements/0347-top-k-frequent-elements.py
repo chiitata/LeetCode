a3e2429900d7479d2d90a1e372aa33ad666fe99c
TaichiSenba <chiitata3326@gmail.com>
2026-04-19 15:02:03 +0900

    Time: 7 ms (50.52%), Space: 22.9 MB (45.7%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,10 +1,7 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num = {}
+        num = defaultdict(int)
         for i in nums:
-            if i in num:
-                num[i] += 1
-            else:
-                num[i] = 1
+            num[i] += 1
         ans = heapq.nlargest(k, num, key=num.get)
         return ans
